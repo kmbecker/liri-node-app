f8b4905508b3e770b9ebc2521e4ddf7e2009d99e
Kyle Becker <kmbecker@users.noreply.github.com>
2018-01-09 21:29:40 -0600

    updated keys

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'xEmobGQ75yiEIieuPXs9mUuYh',
+  consumer_secret: 'fXCCAyi9OMgLRfGIkE4uEUD9eIocvkb9kLS7CSf5R5cYCkzxXH',
+  access_token_key: '950927843931906049-PzeTVSvkI1vqSzLD5pelEIsyUEB3FC5',
+  access_token_secret: '1JQOyhBE392M8hpqxYU8WDgcrYgypwa4RQDg95b0j5JWZ',
 }
 
 module.exports = twitterKeys;
